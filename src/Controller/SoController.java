ac64e399b6d1ea7ea8332560d271db6a11857cb8
jorge <jorge@debian.debian>
2018-03-13 18:40:17 -0300

    Primeiro Commit

---
+++ src/Controller/SoController.java
@@ -0,0 +1,14 @@
+package Controller;
+
+public class SoController {
+
+	public SoController() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+	
+	public String so(){
+		return System.getProperty("os.name");
+	}
+
+}
---
+++ src/view/Main.java
@@ -0,0 +1,15 @@
+package view;
+import Controller.SoController;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SoController soControl = new SoController();
+		System.out.println(soControl.so());
+	}
+
+}
